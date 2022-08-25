f7a3aec18cb884f3fdeb50e6904869f6a171d3b5
majaliju <majalijunyc@gmail.com>
2022-08-24 22:28:05 -0400

    updated

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # app/controllers/application_controller.rb
+class ApplicationController < ActionController::API
+  include ActionController::Cookies
+end
 end
